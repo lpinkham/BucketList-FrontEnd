9df4714409c2a43e5b97986e6a84b554ab20c916
lpinkham <lori_pinkham@comcast.net>
2019-08-29 16:21:32 -0400

    Another fix for heroku

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://secret-badlands-58963.herokuapp.com/',
+  production: 'https://secret-badlands-58963.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
